660537e12ed7cc0621005c8c698104db1fb1c8e7
rypatel45 <rypatel45@gmail.com>
2026-06-15 12:13:36 -0400

    website deployed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import pycountry
+import os
 
 app = Flask(__name__)
 app.secret_key = "atlas-secret-key"
@@ -178,4 +179,5 @@ def reset():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
